b252cb99e953bc1839403d18c142df6e2a796b6c
Ludwig Geistlinger <Ludwig.Geistlinger@bio.ifi.lmu.de>
2016-04-19 13:49:29 +0000

    commit v2.1.16
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EnrichmentBrowser@116545 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sbea.R
+++ R/sbea.R
@@ -242,7 +242,7 @@ ora.hyperg <- function(ps, cmat, alpha=0.05)
 
     # determine significance of overlap 
     # based on hypergeom. distribution
-    gs.ps <- 1 - phyper(ovlp.sizes, gs.sizes, uni.sizes, nr.sigs)
+    gs.ps <- phyper(ovlp.sizes-1, gs.sizes, uni.sizes, nr.sigs, lower.tail=FALSE) 
     
     res.tbl <- cbind(gs.sizes, ovlp.sizes, gs.ps)
     colnames(res.tbl) <- c("NR.GENES", "NR.SIG.GENES", config.ebrowser("GSP.COL"))

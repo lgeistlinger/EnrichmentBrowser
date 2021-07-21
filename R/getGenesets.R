e3ba414d93bf1096d0bd5936edc47e78e7d83612
lgeistlinger <ludwig.geistlinger@gmail.com>
2021-07-21 08:44:59 -0400

    getGenesets: revert to default msigdb cat

--- R/getGenesets.R
+++ R/getGenesets.R
@@ -356,8 +356,9 @@ writeGMT <- function(gs, gmt.file)
                         subcat = NA)
 {
     isAvailable("msigdbr", type = "software")
+    cat <- cat[1]
     supp.cats <- unique(msigdbr::msigdbr_collections()$gs_cat)
-    if(!(cat %in% supp.cats))
+    if(!is.character(cat) || !(cat %in% supp.cats))
         stop(gettextf("'cat' should be one of %s", 
                       paste(dQuote(supp.cats), collapse = ", ")), domain = NA)
  
